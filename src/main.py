aab3f5ef821e76e66e60a5fc81f8c028f0468c8d
Zoesads <83200085+Zoesads@users.noreply.github.com>
2022-03-13 13:05:03 +0700

    Update main.py

--- src/main.py
+++ src/main.py
@@ -3,21 +3,28 @@ from analyzer import Analyze
 from vm import VirtualMachine as VMachine
 from sys import argv as input_args
 
+def run(context):
+    output,log, instruction = Analyze(context).get_result()
+    if output == 1:
+        print("ERROR: %s"%log)
+        return
+    vm_output, vm_log = VMachine(instruction).run()
+    if vm_output == 1:
+        print("ERROR: %s"%vm_log)
+
 def main():
     if len(input_args) == 1:
-        print("ERROR: Missing file path")
-        return
+        while 1:
+            context = input("Enter instruction: ")
+            if context == "exit":
+                return
+            run(context)
     file_path = input_args[1]
     if isfile(file_path):
         file = open(file_path)
-        output,log, instruction = Analyze(file.read()).get_result()
+        context = file.read()
         file.close()
-        if output == 1:
-            print("ERROR: %s"%log)
-            return
-        vm_output, vm_log = VMachine(instruction).run()
-        if vm_output == 1:
-            print("ERROR: %s"%vm_log)
+        run(context)
     else:
         print("ERROR: Unknown %s"%file_path)
 
